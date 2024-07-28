d471b03d45c7d320747c5447476624ee240425cb
unas0706 <sk.unas 123@gmail.com>
2024-07-28 19:13:54 +0530

    updated urls

--- backend/Utility/generateJWT.js
+++ backend/Utility/generateJWT.js
@@ -7,10 +7,8 @@ dotenv.config({
 const generateJWT = (user, res, message, statusCode) => {
   let cokkieName = user.role === "admin" ? "adminToken" : "userToken";
   let token = user.generateJWT();
-  console.log(token);
   res
     .cookie(cokkieName, token, {
-      httpOnly: true, // Prevents JavaScript access to the cookie
       secure: true, // Ensures cookies are sent only over HTTPS
       sameSite: "none",
       maxAge: process.env.COOKIE_EXPIRY * 100 * 60 * 60 * 24,
