08665faf35ad0bb54ea6171abe2ce208653f619e
unas0706 <sk.unas 123@gmail.com>
2024-07-31 15:03:07 +0530

    Updated Backend

--- backend/Utility/generateJWT.js
+++ backend/Utility/generateJWT.js
@@ -7,15 +7,21 @@ dotenv.config({
 const generateJWT = (user, res, message, statusCode) => {
   let cokkieName = user.role === "admin" ? "adminToken" : "userToken";
   let token = user.generateJWT();
+  res.cookie("authToken", "yourAuthToken", {
+    secure: true, // Only set cookie over HTTPS
+    sameSite: "None", // Allow cross-site cookies
+    maxAge: 24 * 60 * 60 * 1000, // 1 day in milliseconds
+  });
+
   res
-    .cookie(cokkieName, token, {
-      secure: true, // Only set cookie over HTTPS
-      sameSite: "None", // Allow cross-site cookies
-      maxAge: 24 * 60 * 60 * 1000, // 1 day in milliseconds
-      // sameSite: "None",
-      // secure: true, // Ensure HTTPS is used
-      // maxAge: process.env.COOKIE_EXPIRY * 1000 * 60 * 60 * 24,
-    })
+    // .cookie(cokkieName, token, {
+    //   secure: true, // Only set cookie over HTTPS
+    //   sameSite: "None", // Allow cross-site cookies
+    //   maxAge: 24 * 60 * 60 * 1000, // 1 day in milliseconds
+    //   // sameSite: "None",
+    //   // secure: true, // Ensure HTTPS is used
+    //   // maxAge: process.env.COOKIE_EXPIRY * 1000 * 60 * 60 * 24,
+    // })
     .status(statusCode)
     .json({
       sucess: true,
