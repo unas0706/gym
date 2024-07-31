b4413a628b729f2296a7a7021733e3670949002e
unas0706 <sk.unas 123@gmail.com>
2024-07-31 13:51:01 +0530

    Updated Backend

--- backend/Utility/generateJWT.js
+++ backend/Utility/generateJWT.js
@@ -7,6 +7,12 @@ dotenv.config({
 const generateJWT = (user, res, message, statusCode) => {
   let cokkieName = user.role === "admin" ? "adminToken" : "userToken";
   let token = user.generateJWT();
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://gym-frontend-56h0.onrender.com"
+  ); // Replace with your client URL
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res
     .cookie(cokkieName, token, {
       sameSite: "None",
