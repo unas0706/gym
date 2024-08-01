94b1e0d5fca0f646d7faa62cee3b5fa6131f8407
unas0706 <sk.unas 123@gmail.com>
2024-08-01 11:06:20 +0530

    Updated Frontend

--- backend/Utility/generateJWT.js
+++ backend/Utility/generateJWT.js
@@ -15,12 +15,18 @@ const generateJWT = (user, res, message, statusCode) => {
 
   res
     .cookie(cokkieName, token, {
-      secure: true, // Only set cookie over HTTPS
-      sameSite: "None", // Allow cross-site cookies
-      maxAge: 24 * 60 * 60 * 1000, // 1 day in milliseconds
-      // sameSite: "None",
-      // secure: true, // Ensure HTTPS is used
-      // maxAge: process.env.COOKIE_EXPIRY * 1000 * 60 * 60 * 24,
+      httpOnly: true, // Makes the cookie inaccessible to JavaScript (XSS protection)
+      secure: true, // Ensures the cookie is only sent over HTTPS
+      sameSite: "None", // Allows cross-site usage
+      maxAge: 24 * 60 * 60 * 1000, // Cookie expiration in milliseconds (1 day here)
+      domain: "gym-frontend-56h0.onrender.com", // Specify the domain if needed
+      path: "/", // Specify the path if needed
+      // secure: true, // Only set cookie over HTTPS
+      // sameSite: "None", // Allow cross-site cookies
+      // maxAge: 24 * 60 * 60 * 1000, // 1 day in milliseconds
+      // // sameSite: "None",
+      // // secure: true, // Ensure HTTPS is used
+      // // maxAge: process.env.COOKIE_EXPIRY * 1000 * 60 * 60 * 24,
     })
     .status(statusCode)
     .json({
